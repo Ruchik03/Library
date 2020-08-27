7bdddef391af9f541d23901d10e607e551785281
Ruchik Chaudhari <ruchikchaudhari@gmail.com>
2020-08-27 16:11:00 -0700

    formatted the code and added cooments

--- script/main.js
+++ script/main.js
@@ -1,50 +1,50 @@
-//reference to buttons
+/**
+ * My library application lets the user store books with the title name, 
+ * author and pages. All the stored books are displayed on the application with
+ * the book details a delete button with it, incase user wants to remove any books. 
+ * NOTE: The application is not provided with any type of storage.
+ * Date: 08/10/2020
+ * @author Ruchik Chaudhari
+ */
+
+//reference to all the buttons
 const addBookbtn = document.querySelector('#addBook');
 const cancelbtn = document.querySelector('#cancel');
 const addbtn = document.querySelector('#addBookM');
-
-
 
 //reference to modal and my library 
 const modal = document.querySelector('#modal');
 const myLibraryDiv = document.querySelector('#myLibrary');
 hideModal();
 
+//store all the books in an array
 let myLibrary = [];
 
 //create a Book object constructor
 class Book {
-    constructor(bookName, author, pages, read) {
+    constructor(bookName, author, pages) {
         this.bookName = bookName,
         this.author = author,
-        this.pages = pages,
-        this.read = read;
+        this.pages = pages
+        //this.read = read;
     }
 }
 
 //for show case create two books 
-
-
 showcase();
-
 function showcase(){
-    let book = new Book('Harry Potter','ruchik chaudhari','450','true');
-    let book1 = new Book('example book','example author','43','read');
-    //let book2 = new Book('example book','example author','43','read');
+    let book = new Book('Game of Thrones','George R.R. Martin','694');
+    let book1 = new Book('To Kill a Mockingbird','Harper Lee','281');
     myLibrary.push(book,book1);
+
     for(let i = 0; i < myLibrary.length;i++){
 
         displayMyLibrary(myLibrary[i],i);
     }
     updateDeletebtn();
- 
 }
  
-//  let book3 = new Book('example book','example author','43','read');
-//  let book4 = new Book('example book','example author','43','read');
-//  let book5 = new Book('example book','example author','43','read');
-//  let book6 = new Book('example book','example author','43','read');
- 
+//update the delete the 
 function updateDeletebtn(){
 
     const deletebtn=document.querySelectorAll('.deleteIcon');
@@ -63,12 +63,11 @@ function updateDeletebtn(){
 addBookbtn.addEventListener('click', showModal);
 
 
-//User interacts with the modal
-
+//User interaction with the modal
 //user enters cancel button-->hide the modal
 cancelbtn.addEventListener('click', hideModal);
 
-//user enter add button--> add the book to my libary array
+//user enters add button--> add the book to my libary array
 addbtn.addEventListener('click', addBookToMyLibrary);
 
 
@@ -76,40 +75,46 @@ addbtn.addEventListener('click', addBookToMyLibrary);
 
 
 
-
+/**
+ * The function gets the information from the user and
+ * creates a new book object from that information.
+ * The object is then pushed to the array and other 
+ * functions are called to update the UI.
+ */
 function addBookToMyLibrary(){
 
     let title = '', author = '', pages = '', read = '';
 
-    //get the complete information from the user
+    //get the information from the user
     title = document.getElementById('title').value;
     author = document.getElementById('author').value;
     pages = document.getElementById('pages').value;
-    read = document.getElementById('radioTxt').value;
-
-    let addBook = new Book (title, author, pages, 'literalRead'); 
-    console.log(addBook);
-    //add the book to my library array
+   
+    let addBook = new Book (title, author, pages); 
     myLibrary.push(addBook);
     hideModal();
+
     displayMyLibrary(addBook,myLibrary.length-1);
     updateDeletebtn();
 
 }
 
+/**
+ * The function creates a new div element and it's child
+ * elements and together they represent the book with
+ * its inforamtion.
+ * @param {*} newBook 
+ * @param {*} id 
+ */
 function displayMyLibrary(newBook,id){
     
 
-
-    //get each object in the array
-    
-
-        //get the data of each object 
-
-
-        //create a div element
+        //create a div element--> This is the parent element 
+        //which holds all the other elements 
         let myBookDiv = document.createElement('div');
-       // let idName = 'book '+myLibrary.length-1;
+
+        //set it's id --> The id is always the very last index 
+        //of the my library array.
         myBookDiv.setAttribute('id',id);
         myBookDiv.classList.add('myBooks'); 
 
@@ -118,64 +123,65 @@ function displayMyLibrary(newBook,id){
         let spanTitle = document.createElement('span');
         let spanAuthor = document.createElement('span');
         let spanPages = document.createElement('span');
-        let spanRead = document.createElement('span');
+        //add a delete button
         let btnDelete = document.createElement('BUTTON');
-
         btnDelete.classList.add('deleteIcon');
 
-
+        //give value to all the elements 
         spanTitle.innerHTML = newBook.bookName;
         spanAuthor.innerHTML = 'By: '+ newBook.author;
         spanPages.innerHTML = 'Pages: '+newBook.pages;
-        spanRead.innerHTML = read;
         btnDelete.innerHTML = '<img src="images/deleteIcon.png">';
 
-
-
-        
-        //add the elements to their parent elements
+        //add all the elements to their parent element
         myBookDiv.appendChild(spanTitle);
         myBookDiv.appendChild(spanAuthor);
         myBookDiv.appendChild(spanPages);
-        myBookDiv.appendChild(spanRead);
         myBookDiv.appendChild(btnDelete);
-
         myLibraryDiv.appendChild(myBookDiv);
 
     
 }
+
+/**
+ * Removes the book from the application
+ * @param {*} divId 
+ */
 function displayRemoveBook(divId){
 
     myLibraryDiv.removeChild(document.getElementById(divId));
 
 }
+
+
+/**
+ * Deletes the specific book from the array and the application
+ * @param {*} event 
+ */
 function deleteBook(event){
     
    //get the #id of the div which represent the book
-   let divId = event.target.parentNode.parentNode.id;
-
-   //all the id's are in format 'book [index]'
-   //get the index by getting the end of the id --> 'book 10' --> '10'
-   
+    let divId = event.target.parentNode.parentNode.id;
     myLibrary.splice(divId,1);
     displayRemoveBook(divId);
-    //alert(myLibrary.length);
 }
 
 
 
 
 
-
-
+/**
+ * Hides the modal
+ */
 function hideModal(){
     modal.style.display = 'none';
     addBookbtn.disabled = false
 }
+/**
+ * Shows the modal
+ */
 function showModal(){
     modal.style.display = 'block';
     addBookbtn.disabled = true;
    // myLibraryDiv.style.display='none';
 }
-
-//myLibraryDiv.style.display='block';
