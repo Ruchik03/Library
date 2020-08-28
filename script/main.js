30eea76b7ed732fa6ff0ba51a3c53c95ae897878
Ruchik03 <63090507+Ruchik03@users.noreply.github.com>
2020-08-27 17:18:23 -0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 /**
  * My library application lets the user store books with the title name, 
  * author and pages. All the stored books are displayed on the application with
- * the book details a delete button with it, incase user wants to remove any books. 
+ * the book details and a delete button with it, incase user wants to remove any books. 
  * NOTE: The application is not provided with any type of storage.
  * Date: 08/10/2020
  * @author Ruchik Chaudhari
